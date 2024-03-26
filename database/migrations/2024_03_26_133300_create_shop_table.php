6af58d9659174fcf9039fff9b39e1bc3df0af20c
SangNef <sang.tv2614@gmail.com>
2024-03-26 15:32:14 +0700

    add shop table

---
+++ database/migrations/2024_03_26_133300_create_shop_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shop', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('reward_id');
+            $table->foreign('reward_id')->references('id')->on('rewards');
+            $table->integer('price');
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shop');
+    }
+}
